9a860db616884a0aae2c8eeeac84d9ed62bbf4bf
juconghe <hejucong1995@gmail.com>
2018-04-01 15:14:12 -0400

    push lab5 demo

--- lab5/Lab5_demo/Lab5_demo.ino
+++ lab5/Lab5_demo/Lab5_demo.ino
@@ -16,28 +16,75 @@ Arduino 1.6.7
 ******************************************************************************/
 #define REED_PIN  14
 #define LED_PIN  15
+#define PIN_BUTTON 0
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "80a0eed50c01403983e52180020c0beb";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "AHHA Lab Wifi";
+char pass[] = "ramanujan";
+int isAlarmOn = 0;
+bool isBtnPush = false;
+BlynkTimer timer;
 
 void setup() 
 {
   Serial.begin(115200);
-  // Since the other end of the reed switch is connected to ground, we need
-  // to pull-up the reed switch pin internally.
+  Blynk.begin(auth, ssid, pass);
+  attachInterrupt(digitalPinToInterrupt(PIN_BUTTON), disable_alram, RISING);
   pinMode(REED_PIN, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
+  timer.setInterval(1000L, blinkLED);
+}
+
+BLYNK_WRITE(V0)
+{
+  int pinValue = param.asInt();
+  // You can also use:
+  // String i = param.asStr();
+  // double d = param.asDouble();
+  isAlarmOn = pinValue;
+  if (pinValue == 0) {
+    isBtnPush = true;
+  }
+  
+}
+
+void disable_alram() {
+  isBtnPush = true;
+  isAlarmOn = 0;
+}
+
+void blinkLED()
+{
+  if (isAlarmOn == 0) {
+    Serial.println("LED on V1: off");
+  } else {
+    Serial.println("LED on V1: on");
+  }
 }
 
 void loop() 
 {
-  digitalWrite(LED_PIN, HIGH); // Turn the LED on
-  int proximity = digitalRead(REED_PIN); // Read the state of the switch
-  if (proximity == LOW) // If the pin reads low, the switch is closed.
+  Blynk.run();
+  timer.run();
+  if (digitalRead(REED_PIN) == LOW) // If the pin reads low, the switch is closed.
   {
-    Serial.println("Switch closed");
+    isAlarmOn = 0;
+    isBtnPush = false;
   }
   else
   {
-    analogWrite(LED_PIN, 127);
-    delay(1000);
+    if (isBtnPush == false) {
+       isAlarmOn = 1;
+    }
   }
 }
 
