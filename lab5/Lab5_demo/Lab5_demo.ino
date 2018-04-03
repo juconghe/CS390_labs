4c0133b1f2f2fbe320b7f6749bf626946af831b6
juconghe <hejucong1995@gmail.com>
2018-04-03 17:22:59 -0400

    finished notification

--- lab5/Lab5_demo/Lab5_demo.ino
+++ lab5/Lab5_demo/Lab5_demo.ino
@@ -2,6 +2,8 @@
 #define REED_PIN  14
 #define LED_PIN  15
 #define PIN_BUTTON 0
+#define PIN_BUZZER 13
+
 #define BLYNK_PRINT Serial
 
 #include <ESP8266WiFi.h>
@@ -9,8 +11,15 @@
 #include <WiFiClient.h>
 #include <Temboo.h>
 #include "TembooAccount.h" // contains Temboo account information, as described below
-WiFiClient client;
+#include <time.h>
+#include <Adafruit_NeoPixel.h>
 
+
+WiFiClient client;
+Adafruit_NeoPixel pixel = Adafruit_NeoPixel(1, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+int timezone = -4;
+int dst = 0;
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
@@ -18,13 +27,14 @@ char auth[] = "80a0eed50c01403983e52180020c0beb";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "AHHA Lab Wifi";
-char pass[] = "ramanujan";
+char ssid[] = "CS390N";
+char pass[] = "internetofthings";
 
 bool isDoorOpen = true;
 int isAlarmOn = 0;
 bool isBtnPush = false;
 bool writeDoorClose = false;
+bool LEDOn = true;
 
 BlynkTimer timer;
 
@@ -34,6 +44,9 @@ void setup()
   Serial.begin(115200);
 
   //Blynk Setup
+  pixel.begin();
+  // lower brightness (default is 256)
+  pixel.setBrightness(128);
   Blynk.begin(auth, ssid, pass);
   attachInterrupt(digitalPinToInterrupt(PIN_BUTTON), disable_alram, RISING);
   pinMode(REED_PIN, INPUT_PULLUP);
@@ -42,7 +55,7 @@ void setup()
 
  //Tamboo Setup
   Serial.println();
-  WiFi.begin("AHHA Lab Wifi", "ramanujan"); //connecting to the router Serial.print("Connecting");
+  WiFi.begin("CS390N", "internetofthings"); //connecting to the router Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
   { 
     delay(500);
@@ -50,7 +63,15 @@ void setup()
   } 
   Serial.println();
   Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP()); 
+  Serial.println(WiFi.localIP());
+
+  configTime(timezone * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  Serial.println("\nWaiting for time");
+  while (!time(nullptr)) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
 }
 
 BLYNK_WRITE(V0)
@@ -61,10 +82,17 @@ BLYNK_WRITE(V0)
   // double d = param.asDouble();
   isAlarmOn = pinValue;
   if (pinValue == 0) {
+    Blynk.virtualWrite(V1, "Alarm Off");
     isBtnPush = true;
   } else {
+    Blynk.virtualWrite(V1, "Alarm On");
+    write_google_spreadsheet("Alarm on");
     isBtnPush = false;
   }
+}
+
+BLYNK_WRITE(V3) {
+  write_google_spreadsheet("Called Police");
 }
 
 void disable_alram() {
@@ -74,10 +102,20 @@ void disable_alram() {
 
 void blinkLED()
 {
-  if (isAlarmOn == 0) {
-    Serial.println("LED on V1: off");
+  if (isAlarmOn == 1) {
+    if (LEDOn) {
+      pixel.setPixelColor(0, pixel.Color(255, 0, 0));
+      pixel.show();
+      LEDOn = !LEDOn;
+    } else {
+      pixel.setPixelColor(0, pixel.Color(0, 0, 0));
+      pixel.show();
+      LEDOn = !LEDOn;
+    }
   } else {
-    Serial.println("LED on V1: on");
+    pixel.setPixelColor(0, pixel.Color(0, 0, 0));
+    pixel.show();
+    LEDOn = true;
   }
 }
 
@@ -92,17 +130,29 @@ void loop()
     isBtnPush = false;
     isDoorOpen = true;
     writeDoorClose = false;
+    Blynk.virtualWrite(V1, "Alarm On");
+    Blynk.virtualWrite(V2, "Door Open");
+    Blynk.notify("Someone open the door!!!!");
     write_google_spreadsheet("Door Open"); // write to google spreadsheet indicate door is open
     write_google_spreadsheet("Alarm on"); // write to google spreadsheet indicate alarm is on
   } else if ((isAlarmOn == 0) && isBtnPush) { // alarm is disable by pressed btn
     Serial.println("Alarm is Disable by clicking btn");
     isBtnPush = false;
+    Blynk.virtualWrite(V1, "Alarm Off");
     write_google_spreadsheet("Alarm Disable"); // write to google spreadsheet indicate alarm is disable
   } else if (proximity == LOW && !writeDoorClose) {
     Serial.println("Door is close");
     isDoorOpen = false;
     writeDoorClose = true;
+    Blynk.virtualWrite(V2, "Door Close");
     write_google_spreadsheet("Door close"); //write to google spreadsheet indicate door is close
+  }
+
+  if (isAlarmOn == 1) {
+      analogWrite(PIN_BUZZER, 512);
+      analogWriteFreq(138);
+  }else {
+      analogWrite(PIN_BUZZER, 0);
   }
 }
 
@@ -110,7 +160,9 @@ void loop()
 void write_google_spreadsheet(String msgType)
 {
    Serial.println("Writing to Google Spereadsheet");
-   String tembooInput = "[[\"100\",\"200\",\"" + msgType + "\"]]";
+   time_t now = time(nullptr);
+   String temp = ctime(&now);
+   String tembooInput = "[[\""+ temp + "\",\"" + msgType + "\"]]";
    TembooChoreo AppendValuesChoreo(client);
     // Invoke the Temboo client
     AppendValuesChoreo.begin();
